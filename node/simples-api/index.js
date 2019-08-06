71e9f2ee10aa717b08e8a3f6709308bb8c420a9d
Lucas Lopes <l.lopes.fga@gmail.com>
2019-08-06 16:21:13 -0300

    fixed

--- node/simples-api/index.js
+++ node/simples-api/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const body_parser = require('body-parser')
+
 const route_users = require('./routes/users')
 
 const app = express()
@@ -7,6 +8,5 @@ const port = 3000
 
 app.use(body_parser.urlencoded({extended:false }))
 route_users(app)
-app.get('/',(req,res)=>{
-    res.send('home')
-}).listen(port, ()=> console.log(`Servido iniciado localhost:${port}`))
+app.get('/',(req,res)=>{res.send('home')})
+    .listen(port, ()=> console.log(`Servido iniciado localhost:${port}`))
--- node/simples-api/routes/users.js
+++ node/simples-api/routes/users.js
@@ -17,17 +17,40 @@ const saveUser = (user)=>{
     fs.writeFileSync(file_path,JSON.stringify(user,null,'\t'))
 }
 const userRoute = (app)=>{
-    app.route('/user/:id')
+    app.route('/user/:id?')
         .get((req,res)=>{
             const users = getUsers()
             res.send({users})
+
         })
         .post((req,res)=>{
             const users = getUsers()
+            // const {email, telefone} = req.body
+
             users.push(req.body)
             saveUser(users)
-            res.status(200).send("Post OK")
+            res.status(201).send("POST OK")
 
+        })
+        .put((req,res)=>{
+            const users = getUsers()
+            saveUser(users.map(user =>{
+                if( user.id === req.params.id){
+                    return{
+                        ...user,
+                        ...req.body
+                    }
+                }
+
+                return user
+            }))
+            res.status(200).send("PUT OK")
+        })
+        .delete((req,res)=>{
+            const users = getUsers()
+            saveUser(users.filter(u => u.id !== req.params.id))
+
+            res.status(200).send("DELETE OK")
         })
 }
 module.exports = userRoute
